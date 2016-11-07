55aa49dc71c09174985f51fa24c66f3e860bc31d
takatori <takatorisatoshi@team-lab.com>
2016-11-07 23:17:55 +0900

    Enable fetch kinesis streams

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate rusoto;
+
+use std::default::Default;
+
+use rusoto::kinesis::{KinesisClient, ListStreamsInput};
+use rusoto::{DefaultCredentialsProvider, Region};
+
 fn main() {
-    println!("Hello, world!");
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = KinesisClient::new(credentials, Region::ApNortheast1);
+    let request = ListStreamsInput::default();
+
+    match client.list_streams(&request) {
+        Ok(output) => {
+            println!("{:?}", output);
+        }
+        Err(error) => {
+            println!("Error: {:?}", error);
+        }
+    };
 }
