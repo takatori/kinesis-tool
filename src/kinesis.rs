dbcd26fea2c95920557420028c3464e1dc6e78af
takatori <takatorisatoshi@team-lab.com>
2016-11-10 02:00:48 +0900

    Separate kineisi helper code from main

---
+++ src/kinesis.rs
@@ -0,0 +1,42 @@
+extern crate rusoto;
+
+use std::error::Error;
+use std::default::Default;
+
+use rusoto::{
+    ProvideAwsCredentials,
+    DispatchSignedRequest,    
+    Region
+};
+use rusoto::kinesis::{
+    KinesisClient,
+    ListStreamsInput
+};
+
+
+pub struct KinesisHelper<P, D> where P: ProvideAwsCredentials, D: DispatchSignedRequest {
+    client: KinesisClient<P, D>
+}
+
+impl<P: ProvideAwsCredentials, D: DispatchSignedRequest> KinesisHelper<P, D> {
+    
+    pub fn new(request_dispatcher: D, credentials_provider: P, region: Region) -> KinesisHelper<P, D> {
+        
+        KinesisHelper { client: KinesisClient::with_request_dispatcher(request_dispatcher, credentials_provider, region) }
+    }
+    
+    pub fn list_streams(&self) {
+        
+        let request = ListStreamsInput::default();
+
+        match self.client.list_streams(&request) {
+            Ok(output) => {
+                println!("{:?}", output);
+            }
+            Err(error) => {
+                println!("Error: {:?}", error);
+            }
+        };        
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
 extern crate rusoto;
 extern crate rustbox;
+extern crate hyper;
 
+mod kinesis;
+    
 use std::error::Error;
 use std::default::Default;
 
 use rusoto::kinesis::{KinesisClient, ListStreamsInput};
 use rusoto::{DefaultCredentialsProvider, ProvideAwsCredentials, DispatchSignedRequest, Region};
 
-
 use rustbox::{Color, RustBox};
 use rustbox::Key;
 
+use kinesis::KinesisHelper;
 
-fn list_streams<P: ProvideAwsCredentials, D: DispatchSignedRequest>(client: &KinesisClient<P, D>) {
-
-    let request = ListStreamsInput::default();
-
-    match client.list_streams(&request) {
-        Ok(output) => {
-            println!("{:?}", output);
-        }
-        Err(error) => {
-            println!("Error: {:?}", error);
-        }
-    };
-}
-
+use hyper::Client;
 
 fn main() {
     
-    let credentials = DefaultCredentialsProvider::new().unwrap();
-    let client = KinesisClient::new(credentials, Region::ApNortheast1);
+    let credentials    = DefaultCredentialsProvider::new().unwrap();
+    let client = Client::new();    
+    let kinesis_helper = KinesisHelper::new(client, credentials, Region::ApNortheast1);
+
 
     let rustbox = match RustBox::init(Default::default()) {
         Result::Ok(v) => v,
@@ -42,17 +34,22 @@ fn main() {
     rustbox.present();
 
     loop {
+        
+        rustbox.clear();
+
         match rustbox.poll_event(false) {
             Ok(rustbox::Event::KeyEvent(key)) => {
                 match key {
                     Key::Char('q') => { break; }
-                    Key::Char('l') => { list_streams(&client); }
-                    _ => { }
+                    Key::Char('l') => { kinesis_helper.list_streams(); }
+                    _ => { rustbox.clear(); }
                 }
             },
             Err(e) => panic!("{}", e.description()),
             _ => { }
         }
+
+        
     }
     
 }
