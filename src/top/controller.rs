64f90eef8c59a1683cd1a1f3d771e3fe7c3c3734
takatori <takatorisatoshi@team-lab.com>
2017-04-17 23:58:06 +0900

    Separate header string to const variable

--- src/top/controller.rs
+++ src/top/controller.rs
@@ -4,6 +4,23 @@ use utils::screen::Screen;
 use utils::screen::ScreenStatus;
 use aws::awsController;
 
+const header: &'static str = r###"
+   -----------------------------------------
+
+   ██████╗  █████╗ ██╗    ██╗███████╗████████╗
+   ██╔══██╗██╔══██╗██║    ██║██╔════╝╚══██╔══╝
+   ██████╔╝███████║██║ █╗ ██║███████╗   ██║   
+   ██╔══██╗██╔══██║██║███╗██║╚════██║   ██║   
+   ██║  ██║██║  ██║╚███╔███╔╝███████║   ██║   
+   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝   ╚═╝   
+
+   ------------------------------------------                                           
+
+   rawst is the interactive tool for AWS CLI written in Rust.
+
+   "###;
+
+
 pub struct topController {
     screen: Screen,
 }
@@ -17,21 +34,9 @@ impl topController {
     }
 
     pub fn run(&mut self) {
-        let commands = vec!["a".to_string(), "q".to_string()];
-        self.screen.update_screen(r###"
-   -----------------------------------------
 
-   ██████╗  █████╗ ██╗    ██╗███████╗████████╗
-   ██╔══██╗██╔══██╗██║    ██║██╔════╝╚══██╔══╝
-   ██████╔╝███████║██║ █╗ ██║███████╗   ██║   
-   ██╔══██╗██╔══██║██║███╗██║╚════██║   ██║   
-   ██║  ██║██║  ██║╚███╔███╔╝███████║   ██║   
-   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝   ╚═╝   
-
-   ------------------------------------------                                           
-
-   rawst is the interactive tool for AWS CLI written in Rust.
-   "###, &commands);
+        self.screen.header = header;
+        self.screen.lines  = vec!["aws".to_string(), "q".to_string()];
 
         match self.screen.select_line() {
             ScreenStatus::Error | ScreenStatus::Quit => println!("error"),
